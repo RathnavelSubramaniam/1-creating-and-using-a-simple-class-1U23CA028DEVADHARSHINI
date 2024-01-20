b06ac7f4e0c03d8242a0eff9bec4e69ee767f508
1U23CA028DEVADHARSHINI <139437425+1U23CA028DEVADHARSHINI@users.noreply.github.com>
2024-01-20 04:58:51 +0000

    saved1

--- Person.java
+++ Person.java
@@ -1,30 +1,16 @@
-class Persona{
-    String firstname;
-    String lastname;
-    int age;
-    Persona(String fName,String lName,int a)
-    {
-        firstname = fName;
-        lastname  = lName;
-        age =a;
-    }
-    String getfullname()
-    {
-        return firstname + " " +lastname;
-    }
-}
+
     public class Person
     {
     public static void main(String args[])
     {
-     Persona b1 = new Persona("Alice","bob",30);
-     Persona b2 = new Persona("Alice","bob",25);
+     Persona b1 = new Persona("Jhon","Doe",30);
+     Persona b2 = new Persona("Alice","Smith",25);
 
      String ABC = b1.getfullname();
      String EFG = b2.getfullname();
      double average = ((b1.age + b2.age)/2.0);
-     System.out.println("person 1:"+b1.getfullname());
-     System.out.println("person 2:"+b2.getfullname());
+     System.out.println("Person 1:"+b1.getfullname());
+     System.out.println("Person 2:"+b2.getfullname());
      System.out.println("Average Age:" +average);
     }
     }
